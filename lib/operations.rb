da2b2df73e6d9dfde89233e5f324236dbab987bb
Randall Taylor <randyisbusy@gmail.com>
2019-07-15 20:06:44 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,12 @@
 def unsafe?(speed)
-	speed > 60 || speed < 40 ? true : false
+	if speed > 60 || speed < 40 
+		 true 
+	else
+		 false
 end
 
 
 
 def not_safe?(speed)
-
+	speed > 60 || speed < 30 ? true : false
 end
